4637767fc9577a0e7588e1135c14c928ae49900d
Ludovic Engrand <the.8tre@gmail.com>
2023-05-02 21:55:14 +0200

    Lint + prettier version

--- custom_components/somfy_protexial/__init__.py
+++ custom_components/somfy_protexial/__init__.py
@@ -5,7 +5,13 @@ from datetime import timedelta
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_URL, CONF_USERNAME, Platform
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_SCAN_INTERVAL,
+    CONF_URL,
+    CONF_USERNAME,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
 from homeassistant.helpers.typing import ConfigType
