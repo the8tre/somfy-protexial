69557981732cf6a9f9afb2f0402dfa933e303cce
Ludovic Engrand <the.8tre@gmail.com>
2023-05-05 10:14:15 +0200

    Sort imports

--- custom_components/somfy_protexial/protexial.py
+++ custom_components/somfy_protexial/protexial.py
@@ -1,11 +1,10 @@
 import asyncio
-import logging
 from enum import Enum
+import logging
 from xml.etree import ElementTree as ET
 
+from aiohttp import ClientError, ClientSession
 import async_timeout
-from aiohttp import ClientError
-from aiohttp import ClientSession
 from pyquery import PyQuery as pq
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
