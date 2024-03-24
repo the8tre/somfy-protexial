82b61773c5ee0a6e88a16f42e57c25118ede3b3c
Ludovic Engrand <the.8tre@gmail.com>
2024-03-24 11:37:27 +0100

    Add options flow support

--- custom_components/somfy_protexial/__init__.py
+++ custom_components/somfy_protexial/__init__.py
@@ -95,11 +95,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         DEVICE_INFO: device_info,
     }
 
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+
     await coordinator.async_config_entry_first_refresh()
 
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     )
+
     return True
 
 
@@ -113,3 +116,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle an options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
--- custom_components/somfy_protexial/config_flow.py
+++ custom_components/somfy_protexial/config_flow.py
@@ -1,7 +1,7 @@
 import logging
 import re
 from urllib.parse import urlparse
-
+from typing import Any
 from homeassistant import config_entries
 from homeassistant.components.alarm_control_panel import AlarmControlPanelEntityFeature
 from homeassistant.const import (
@@ -13,6 +13,7 @@ from homeassistant.const import (
     STATE_ALARM_ARMED_HOME,
     STATE_ALARM_ARMED_NIGHT,
 )
+from homeassistant.core import callback
 from homeassistant.helpers import aiohttp_client, config_validation as cv
 from homeassistant.helpers.selector import (
     NumberSelector,
@@ -22,6 +23,7 @@ from homeassistant.helpers.selector import (
     TextSelectorConfig,
     TextSelectorType,
 )
+from homeassistant.data_entry_flow import FlowResult
 import voluptuous as vol
 
 from .const import CONF_ARM_CODE, CONF_CODE, CONF_CODES, CONF_MODES, DOMAIN
@@ -134,6 +136,77 @@ class ProtexialConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         TextSelectorConfig(type=TextSelectorType.PASSWORD)
                     ),
                     vol.Required(CONF_SCAN_INTERVAL, default=60): NumberSelector(
+                        NumberSelectorConfig(
+                            mode=NumberSelectorMode.BOX, min=15, max=3600, step=1
+                        )
+                    ),
+                }
+            ),
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> config_entries.OptionsFlow:
+        """Create the options flow."""
+        return ProtexialOptionsFlowHandler(config_entry)
+
+class ProtexialOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize Protexial options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        errors = {}
+        if user_input is not None:
+            arm_code = (
+                user_input[CONF_ARM_CODE] if CONF_ARM_CODE in user_input else None
+            )
+            if arm_code is None or re.match("^[0-9]{4}$", str(arm_code)):
+                modes = []
+                # if user_input[STATE_ALARM_ARMED_AWAY]:
+                modes.append(AlarmControlPanelEntityFeature.ARM_AWAY)
+                if user_input[STATE_ALARM_ARMED_NIGHT]:
+                    modes.append(AlarmControlPanelEntityFeature.ARM_NIGHT)
+                if user_input[STATE_ALARM_ARMED_HOME]:
+                    modes.append(AlarmControlPanelEntityFeature.ARM_HOME)
+
+                newData = {
+                    CONF_URL: self.config_entry.data[CONF_URL],
+                    CONF_API_TYPE: self.config_entry.data[CONF_API_TYPE],
+                    CONF_USERNAME: self.config_entry.data[CONF_USERNAME],
+                    CONF_PASSWORD: self.config_entry.data[CONF_PASSWORD],
+                    CONF_CODES: self.config_entry.data[CONF_CODES],
+                    CONF_MODES: modes,
+                    CONF_ARM_CODE: arm_code,
+                    CONF_SCAN_INTERVAL: user_input[CONF_SCAN_INTERVAL],
+                    ATTR_SW_VERSION: self.config_entry.data[ATTR_SW_VERSION],
+                }
+                self.hass.config_entries.async_update_entry(
+                    self.config_entry, data=newData, options=self.config_entry.options
+                )
+                return self.async_create_entry(title="", data={})
+            else:
+                errors["base"] = "arm_code"
+
+        currentModes = self.config_entry.data[CONF_MODES]
+        hasHomeMode = any(m == AlarmControlPanelEntityFeature.ARM_HOME for m in currentModes)
+        hasNightMode = any(m == AlarmControlPanelEntityFeature.ARM_NIGHT for m in currentModes)
+        return self.async_show_form(
+            step_id="init",
+            errors=errors,
+            data_schema=vol.Schema(
+                {
+                    # vol.Required(STATE_ALARM_ARMED_AWAY, default=True): cv.boolean,
+                    vol.Optional(STATE_ALARM_ARMED_NIGHT, default=hasNightMode): cv.boolean,
+                    vol.Optional(STATE_ALARM_ARMED_HOME, default=hasHomeMode): cv.boolean,
+                    vol.Optional(CONF_ARM_CODE): TextSelector(
+                        TextSelectorConfig(type=TextSelectorType.PASSWORD)
+                    ),
+                    vol.Required(CONF_SCAN_INTERVAL, default=self.config_entry.data[CONF_SCAN_INTERVAL]): NumberSelector(
                         NumberSelectorConfig(
                             mode=NumberSelectorMode.BOX, min=15, max=3600, step=1
                         )
